071b26e265ddd3c221bc9334ca8a10a784806daf
matt <controlgame@icloud.com>
2022-03-20 23:41:49 -0300

    better

--- main.c
+++ main.c
@@ -33,11 +33,22 @@ Rectangle ballspecs = {
 
 
 blockers blocks[5][10];
-bool colideX(Rectangle rect1, Rectangle rect2){
-    
-}
+// bool colideX(Rectangle rect1, Rectangle rect2){
+//     if( rect1.x == rect2.x + rect2.width  && 
+//         rect1.y <= rect2.y + rect2.height &&
+//          rect1.y + rect1.height >= rect2.y
+//         )
+//         return true;
+//     return false;    
+// }
 bool colideY(Rectangle rect1, Rectangle rect2){
-    if(rect1.y + rect1.height >= rect2.y && rect1.x <= rect2.x + rect2.width && rect1.x + rect1.width >= rect2.x)
+    if( rect1.y + rect1.height == rect2.y &&
+        rect1.x <= rect2.x + rect2.width  &&
+        rect1.x + rect1.width >= rect2.x)
+        return true;
+    else if(rect1.y  == rect2.y + rect2.height &&
+            rect1.x <= rect2.x + rect2.width  &&
+            rect1.x + rect1.width >= rect2.x)
         return true;
     return false;    
 }
@@ -48,9 +59,21 @@ void pysic(Rectangle player, CompleteBall* ball,blockers blocks[5][10],int lifes
     ball->ballCords.x + ball->ballCords.width == width || ball->ballCords.x == 0? ball->spdX = -ball->spdX : 0;
     ball->ballCords.y += ball->spdY;
     ball->ballCords.y + ball->ballCords.height == height || ball->ballCords.y == 0? ball->spdY = -ball->spdY : 0;
-      
+    // if(colideX(ball->ballCords,player))
+    //     ball->spdX = -ball->spdX;
     if(colideY(ball->ballCords,player))
         ball->spdY = -ball->spdY;
+    for(int y = 0; y < 5; y++){
+        for(int x = 0; x < 10; x++){
+            if(blocks[y][x].state == true){
+              if(colideY(ball->ballCords,blocks[y][x].spec)){
+                    blocks[y][x].state = false;
+                    ball->spdY = -ball->spdY;
+                }  
+            }
+            
+        }
+    }
 }
 int main(void){
 
