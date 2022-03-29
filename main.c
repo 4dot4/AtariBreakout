e06e926712f4664d6c0f4a249d93a4b3fbba6f49
matt <controlgame@icloud.com>
2022-03-28 23:10:12 -0300

    added logo

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ const int PlayerSpeed = 15;
 int score  = 0;
 int ballXspeed = 10;
 int ballYspeed = -10;
-
+int start = 0;
 unsigned char canceled = 0;
 
 Rectangle rec = {
@@ -176,9 +176,24 @@ void restartBlocks(int* nivel){
 int main(void){
 
   
-    
+    int logoPositionX = GetScreenWidth()/2 - 128;
+    int logoPositionY = GetScreenHeight()/2 - 128;
+
+    int framesCounter = 0;
+    int lettersCount = 0;
+
+    int topSideRecWidth = 16;
+    int leftSideRecHeight = 16;
+
+    int bottomSideRecWidth = 16;
+    int rightSideRecHeight = 16;
+
+    int state = 0;                  // Tracking animation states (State Machine)
+    float alpha = 1.0f;
+
     int lifes = 5;
     int nivel = GetRandomValue(0,5);
+
     CompleteBall ball = {
         .ballCords = ballspecs,
         .spdX = ballXspeed,
@@ -203,6 +218,55 @@ int main(void){
     while (!WindowShouldClose()){
        
         UpdateMusicStream(music1);
+
+        if (state == 0)                 // State 0: Small box blinking
+        {
+            framesCounter++;
+
+            if (framesCounter == 120)
+            {
+                state = 1;
+                framesCounter = 0;      // Reset counter... will be used later...
+            }
+        }
+        else if (state == 1)            // State 1: Top and left bars growing
+        {
+            topSideRecWidth += 4;
+            leftSideRecHeight += 4;
+
+            if (topSideRecWidth == 256) state = 2;
+        }
+        else if (state == 2)            // State 2: Bottom and right bars growing
+        {
+            bottomSideRecWidth += 4;
+            rightSideRecHeight += 4;
+
+            if (bottomSideRecWidth == 256) state = 3;
+        }
+        else if (state == 3)            // State 3: Letters appearing (one by one)
+        {
+            framesCounter++;
+
+            if (framesCounter/12)       // Every 12 frames, one more letter!
+            {
+                lettersCount++;
+                framesCounter = 0;
+            }
+
+            if (lettersCount >= 10)     // When all letters have appeared, just fade out everything
+            {
+                alpha -= 0.02f;
+
+                if (alpha <= 0.0f)
+                {
+                    alpha = 0.0f;
+                    state = 4;
+                }
+            }
+        }
+        else if(state == 4){ 
+            
+        }
         if(IsKeyPressed(KEY_P)){
             
             
@@ -213,8 +277,9 @@ int main(void){
                 PauseMusicStream(music1);
                 pause = true;
             }
+
         }
-       
+        
         if(!pause){
             if(IsKeyDown(KEY_A))
                 if(player.x > 0)
@@ -223,14 +288,15 @@ int main(void){
                 if(player.x + player.width < width)
                     player.x += PlayerSpeed;
             if(lifes > 0){
-                if(canceled != 50)
-                    pysic(player,&ball,blocks,&lifes);
-                else{
-                    nivel = GetRandomValue(0,5);    
-                    restartBlocks(&nivel);
-                    restartBall(&ball);
+                if(start == 1){
+                    if(canceled != 50)
+                        pysic(player,&ball,blocks,&lifes);
+                    else{
+                        nivel = GetRandomValue(0,5);    
+                        restartBlocks(&nivel);
+                        restartBall(&ball);
+                    }    
                 }    
-                    
             }           
                
         }            
@@ -242,14 +308,50 @@ int main(void){
         BeginDrawing();
 
         ClearBackground(BLACK);
-        
-        if(lifes > 0){
-          for(int y = 0; y < 5;y++){
-            for(int x = 0 ; x < 10; x++){
-                if(blocks[y][x].state == true)
-                    DrawRectangle(blocks[y][x].spec.x,blocks[y][x].spec.y,blocks[y][x].spec.width,blocks[y][x].spec.height,blocks[y][x].blockColor);
-                }
+
+        if(start == 0){
+           if (state == 0)
+            {
+                if ((framesCounter/15)%2) DrawRectangle(logoPositionX, logoPositionY, 16, 16, RAYWHITE);
             }
+            else if (state == 1)
+            {
+                DrawRectangle(logoPositionX, logoPositionY, topSideRecWidth, 16, BLACK);
+                DrawRectangle(logoPositionX, logoPositionY, 16, leftSideRecHeight, BLACK);
+            }
+            else if (state == 2)
+            {
+                DrawRectangle(logoPositionX, logoPositionY, topSideRecWidth, 16, BLACK);
+                DrawRectangle(logoPositionX, logoPositionY, 16, leftSideRecHeight, BLACK);
+
+                DrawRectangle(logoPositionX + 240, logoPositionY, 16, rightSideRecHeight, BLACK);
+                DrawRectangle(logoPositionX, logoPositionY + 240, bottomSideRecWidth, 16, BLACK);
+            }
+            else if (state == 3)
+            {
+                DrawRectangle(logoPositionX, logoPositionY, topSideRecWidth, 16, Fade(BLACK, alpha));
+                DrawRectangle(logoPositionX, logoPositionY + 16, 16, leftSideRecHeight - 32, Fade(BLACK, alpha));
+
+                DrawRectangle(logoPositionX + 240, logoPositionY + 16, 16, rightSideRecHeight - 32, Fade(BLACK, alpha));
+                DrawRectangle(logoPositionX, logoPositionY + 240, bottomSideRecWidth, 16, Fade(BLACK, alpha));
+
+                DrawRectangle(GetScreenWidth()/2 - 112, GetScreenHeight()/2 - 112, 224, 224, Fade(RAYWHITE, alpha));
+
+                DrawText(TextSubtext("raylib", 0, lettersCount), GetScreenWidth()/2 - 44, GetScreenHeight()/2 + 48, 50, Fade(BLACK, alpha));
+            }
+            else if(state == 4){
+                start = 1;
+            } 
+        }
+        
+        if(start == 1){
+          if(lifes > 0){
+            for(int y = 0; y < 5;y++){
+                for(int x = 0 ; x < 10; x++){
+                    if(blocks[y][x].state == true)
+                        DrawRectangle(blocks[y][x].spec.x,blocks[y][x].spec.y,blocks[y][x].spec.width,blocks[y][x].spec.height,blocks[y][x].blockColor);
+                    }
+                }
             DrawText(TextFormat("Score: %d",score),1200,800,40,RAYWHITE);   
             DrawText(TextFormat("%d",lifes),50,800,40,lifeColor);
             DrawRectangle(ball.ballCords.x,ball.ballCords.y,ball.ballCords.width,ball.ballCords.height,RAYWHITE);
@@ -261,7 +363,9 @@ int main(void){
             DrawText("Game Over",580,450,80,RED);
             DrawText(TextFormat("Score: %d",score),680,600,40,RAYWHITE);
             
-        }
+        }  
+        }    
+        
         
         EndDrawing();
     }
